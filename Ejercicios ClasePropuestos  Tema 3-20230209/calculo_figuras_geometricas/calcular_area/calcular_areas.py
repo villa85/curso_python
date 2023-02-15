71b950064b2ca0338e3e94311f7f387f6cef6de8
villa <villalon2511@gmail.com>
2023-02-15 11:38:41 +0100

    cambios

---
+++ Ejercicios ClasePropuestos  Tema 3-20230209/calculo_figuras_geometricas/calcular_area/calcular_areas.py
@@ -0,0 +1,25 @@
+import math
+
+def area_paralelogramo(b, h, cuadrado = False): # con esta funcion calculamos el area de cualquier paralerogramo (cuadrado, rectángulo)
+    if not cuadrado:
+        area_rectangulo = b * h
+        return area_rectangulo
+    elif cuadrado:
+        area_cuadrado = math.pow(b, 2)
+        return area_cuadrado
+
+def area_triangulo(base, altura):
+    return base * altura / 2
+
+def area_rombo(diagonal1, diagonal2):
+    return (diagonal1 * diagonal2) / 2
+
+def area_circulo(radio):
+    return math.pi * radio ** 2
+
+def area_trapecio(B, b, h):
+    return ((B + b) / 2) * h
+
+def area_poligono_regular(cant_lados, medida_lados, apotema):
+    return ((cant_lados * medida_lados) * apotema) / 2
+
---
+++ Ejercicios ClasePropuestos  Tema 3-20230209/calculo_figuras_geometricas/main.py
@@ -0,0 +1,6 @@
+from calcular_area import calcular_areas as ca
+import os
+
+# print(f"{ca.area_circulo(3):.2f}")
+
+print(os.getcwd())
