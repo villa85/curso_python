d1640d79b75b0d4c79250805d112ec8904fc63c4
villa <villalon2511@gmail.com>
2023-02-20 20:03:37 +0100

    Ejercicios terminados todos

--- Ejercicios_ClasePropuestos_Tema_3-20230209/ejercicio_13/funciones_lista.py
+++ Ejercicios_ClasePropuestos_Tema_3-20230209/ejercicio_13/funciones_lista.py
@@ -1,6 +1,33 @@
 def maximo_valor(lista):
-    mayor = 0
+    mayor = lista[0]
     for i in lista:
-        if i > 0:
+        if i > mayor:
             mayor = i
     return mayor
+
+def minimo_valor(lista):
+    menor = lista[0]
+    for i in lista:
+        if i < menor:
+            menor = i
+    return menor
+
+def pares(lista, par = False):
+    lista_pares = []
+    lista_impares = []
+    if par:
+        for i in lista:
+            if i % 2 == 0:
+                lista_pares.append(i)
+        return lista_pares
+    else:
+        for i in lista:
+            if i % 2 != 0:
+                lista_impares.append(i)
+        return lista_impares
+
+def sumatoria(lista):
+    return sum(lista)
+
+def promedio(suma, lista):
+    return suma / len(lista)
--- Ejercicios_ClasePropuestos_Tema_3-20230209/ejercicio_13/main.py
+++ Ejercicios_ClasePropuestos_Tema_3-20230209/ejercicio_13/main.py
@@ -7,4 +7,16 @@ for i in range(10):
     lista.append(random.randint(0, 1000))
 
 maxi = fl.maximo_valor(lista)
-print(maxi)
+minimo = fl.minimo_valor(lista)
+impares = fl.pares(lista)
+pares = fl.pares(lista, par = True)
+sumatoria = fl.sumatoria(lista)
+promedio = fl.promedio(sumatoria, lista)
+
+print(lista)
+print(f"El valor máximo del lista es: {maxi}")
+print(f"El valor minimo del lista es: {minimo}")
+print(f"Hay {len(impares)} números impares")
+print(f"Hay {len(pares)} números impares")
+print(f"La sumatoria es {sumatoria}")
+print(f"El promedio es {promedio}")
